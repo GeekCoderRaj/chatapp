d6c828d832eabb22ecd7d66d7073ee7215885f10
AKHIL RAJ SRIVASTAVA <84132589+AKHILRAJSRIVASTAVA@users.noreply.github.com>
2021-12-21 20:09:56 +0530

    chatapp

---
+++ backened/index.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const express = require('express');
+const cors = require("cors");
+const socketIO = require('socket.io');
+const app = express();
+app.use(cors);
+const PORT = 4000 || process.env.PORT;
+const server= http.createServer(app);
+app.get('/',(req,res)=>{
+    res.send("<h1> I </h1>");
+})
+const users = [{}];
+const io=socketIO(server);
+
+io.on("connection",(socket)=>{
+    console.log("NEW CONNECTION");
+
+    socket.on('joined',({user, room})=>{
+        socket.join(room);
+        users[socket.id]=user;
+        console.log(`${user} has joined`);
+        socket.broadcast.to(room).emit('userJoined',{user:"Admin",message:`${users[socket.id]} has joined`});
+        console.log(users);
+    })
+    socket.on('message',({message,room,id})=>{
+        console.log(message,room);
+        socket.broadcast.to(room).emit('sendMessage',{message,id});
+        console.log("send");
+    })
+    socket.on('disconnect',(room)=>{
+        socket.broadcast.to(room).emit('leave',{user:"Admin",message:`${users[socket.id]} has left`});
+        console.log("user left");
+    });
+    socket.emit('welcome',{user:"Admin",message:'Welcome to the chat'});
+
+})
+
+server.listen(PORT,()=>{
+    console.log(`PORT RUNNING at http://localhost:${PORT}`);
+})
+
